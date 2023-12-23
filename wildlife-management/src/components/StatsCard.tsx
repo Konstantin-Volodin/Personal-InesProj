483d9239dedd7cfe3049baba70bd6210157e50de
Konstantin-Volodin <63243086+Konstantin-Volodin@users.noreply.github.com>
2023-12-22 22:52:59 -0500

    Update StatsCard.tsx

--- wildlife-management/src/components/StatsCard.tsx
+++ wildlife-management/src/components/StatsCard.tsx
@@ -22,7 +22,7 @@ export default function StatsCard(props: Props) {
 
     return (
         <div className='w-28 sm:w-36 flex flex-col drop-shadow-xl place-items-center
-                        transition-all hover:scale-110'>
+                        transition-all hover:scale-110 cursor-pointer'>
 
             <div className='w-full h-28 sm:h-36 border-4 border-slate-700 mb-1 bg-no-repeat bg-cover bg-center bg-top'
                 style={{ backgroundImage: 'url(' + img + ')' }}
